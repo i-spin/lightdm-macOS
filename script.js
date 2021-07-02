2e337ca4d1127cf27a8418ea3123abaf1ce72541
sourtaste000 <sourtaste000@gmail.com>
2021-07-02 11:08:54 -0700

    removed deprecated functions

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
-for (var i = 0; i < lightdm.sessions.length; i++) {
-    var session = lightdm.sessions[i];
-    var s = document.createElement('option');
-    s.innerHTML = session.name;
-    s.setAttribute("value", session.key);
-    s.id = "s" + i;
+setInterval(() => {
+    lightdm.authenticate(document.getElementById("#usrname").value)
+}, 1000)
 
-    document.getElementById("sessions").appendChild(s);
-}
+$("#time").replaceWith(new Date().toDateString())
 
 $("#passwd").on('keyup', (e) => {
     if (e.key === 'Enter' || e.keyCode === 13) {
-        lightdm.provide_secret(document.getElementById("passwd").value);
-        lightdm.start_authentication(document.getElementById("usrname").value);
+        lightdm.respond(document.getElementById("passwd").value)
     }
 })
 
+
+/**
+ * This will be called when LightDM needs to prompt the user for some reason, 
+ * such as asking for a password. The "text" parameter will be the text of the prompt, 
+ * and the "type" parameter will either be "text" for a visible prompt, 
+ * or "password" for a prompt that the input should be hidden.
+ * @param {String} text 
+ * @param {String} type 
+ */
+function show_prompt(text, type) {}
+
+
+/**
+ * This will be called when LightDM needs to display some info for the user. 
+ * The "text" parameter will be the text of the message, and the "type" parameter will either be 
+ * "info" for an information message, or "error" for an error message that LightDM has encountered.
+ * @param {String} text 
+ * @param {String} type
+ */
+function show_message(text, type) {}
+
+
+/**
+ * This function is called by LightDM when authentication has completed.
+ */
 function authentication_complete() {
-    if (lightdm.is_authenticated) {
-        /** Select session and log in */
-        var session = document.getElementById('sessions').value;
-        show_message("Correct Password, Logging in", "success");
-        lightdm.login (lightdm.authentication_user, session);
-    } else {
-        /** Shows error message, starts auth process again */
-        show_message("Incorrect Password", "error");
-        start_auth();
-    }
-};
+    lightdm.start_session_sync(session)
+}
+
+
+/**
+ * This function is called by LightDM when an autologin user’s login timer has expired. 
+ * The greeter should reset the authentication process.
+ */
+function autologin_timer_expired() {}
+
